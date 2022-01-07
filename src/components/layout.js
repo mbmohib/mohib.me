24a06deb18e6bb78368540673111821a50044690
Mohib <mbmohib@gmail.com>
2022-01-07 15:42:50 +0600

    handle react not defined error

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,8 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import Header from './header';
+import '../style/main.css';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,7 +13,7 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
@@ -41,11 +33,11 @@ const Layout = ({ children }) => {
         </footer>
       </div>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
 // tailwind.config.js
 module.exports = {
   future: {},
-  purge: [],
-  theme: {
-    extend: {},
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  colors: {
+    primary: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: {
+        default: '#ccc',
+        100: '#7e5bef',
+      },
+    },
+    pink: '#ff49db',
+    orange: '#ff7849',
+    green: '#13ce66',
+    yellow: '#ffc82c',
+    'gray-dark': '#273444',
+    gray: '#8492a6',
+    'gray-light': '#d3dce6',
+  },
+  fontFamily: {
+    sans: ['Graphik', 'sans-serif'],
+    serif: ['Merriweather', 'serif'],
   },
   variants: {},
   plugins: [],
-}
+};
