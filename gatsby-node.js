6bb5caf8da24584d4ed0f70f853361e93aff82af
Mohib <mbmohib@gmail.com>
2022-01-27 20:15:14 +0600

    👌 IMPROVE: add first draft of redux devtool article

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,25 +14,9 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 
   const blogPostTemplate = path.resolve(`src/templates/blog.tsx`);
 
-  const blogPosts = await graphql(`
-    query {
-      production: allMarkdownRemark(
-        filter: {
-          fileAbsolutePath: { regex: "/posts/" }
-          frontmatter: { status: { eq: "published" } }
-        }
-        sort: { order: DESC, fields: [frontmatter___createdAt] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              path
-            }
-          }
-        }
-      }
-      development: allMarkdownRemark(
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
         filter: { fileAbsolutePath: { regex: "/posts/" } }
         sort: { order: DESC, fields: [frontmatter___createdAt] }
         limit: 1000
@@ -48,14 +32,12 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     }
   `);
 
-  if (blogPosts.errors) {
+  if (result.errors) {
     reporter.panicOnBuild(`Error while running GraphQL query.`);
     return;
   }
 
-  const environment = process.env.NODE_ENV;
-
-  blogPosts.data[environment].edges.forEach(({ node }) => {
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: `/blog/${node.frontmatter.path}`,
       component: blogPostTemplate,
